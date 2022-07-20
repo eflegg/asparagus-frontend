121145b3305874a665c365de7e9a413c1e2446dc
Elizabeth Flegg <elizabeth@MacBook-Pro-de-admin.local>
2022-07-20 16:02:38 -0400

    merging staging

---
+++ pages/styleguide.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styled from "styled-components";
+import theme from "../components/Global/Theme";
+
+
+export default function Styleguide(){
+    return (
+    <div className="styledguide—container">
+    more code goes here
+    </div>
+    )
+    }
