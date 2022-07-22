40cef9e8a4e1e5207fce1734561ed111eef355c4
Erin Flegg <ehflegg@gmail.com>
2022-07-22 13:46:55 -0700

    merge in staging

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -3,9 +3,6 @@ import styled from "styled-components";
 import theme from "../components/Global/Theme";
 const Card = styled.div`
   border: 2px solid ${theme.colours.grey};
-  a {
-    text-decoration: none;
-  }
 `;
 
 // needs categories, reading time, date, image
@@ -21,11 +18,13 @@ export default function ArticleCard({
     <Card>
       <Link href={"/articles/[slug]"} as={`/articles/${slug}`}>
         <a>
+          <p className="categories">
+            <span></span>
+          </p>
           <h3
             className="card-text pb-5"
             dangerouslySetInnerHTML={{ __html: title }}
           ></h3>
-          <p className="deck--article-card">I'm the deck</p>
           <p className="writer">{writer}</p>
           <p className="photographer">{photographer}</p>
           <p className="excerpt">{excerpt}</p>
