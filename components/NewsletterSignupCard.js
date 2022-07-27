4d210c1c0627770abef7e733b6382808dc25ad36
Erin Flegg <ehflegg@gmail.com>
2022-07-26 17:26:21 -0700

    added text--support class with & operator to change border colour on SignUp styled component

--- components/NewsletterSignupCard.js
+++ components/NewsletterSignupCard.js
@@ -52,7 +52,10 @@ const PhotoStyled = styled.div`
 `;
 
 const SignUp = styled.div`
-  border: solid green;
+  border: 3px solid mediumseagreen;
+  &.text--support {
+    border: 3px solid rebeccapurple;
+  }
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -76,7 +79,7 @@ export default function NewsletterSignup({ title, subtitle, image, support }) {
     <NewsletterBlock>
       {/* @elizabeth this is how you use a prop in a ternary. if support exists, add this class. otherwise do nothing. apply signupWrapper regardless */}
       <div className={`${support ? "wrapper--support" : " "} signupWrapper`}>
-        <SignUp>
+        <SignUp className={`${support ? "text--support" : " "} `}>
           <p className="newsletter-header--primary">{title}</p>
           <p className="newsletter-subheader--primary">{subtitle}</p>
           <div className="flex-row">
