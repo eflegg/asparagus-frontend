a1748365d567ad08f0d20337a29abd109f6d9ca7
Elizabeth Flegg <elizabeth.flegg@gmail.com>
2022-08-01 17:43:20 -0400

    new file

---
+++ pages/stockists/[slug].js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import { getEvent, getSlugs } from "../../utils/wordpress";
+import theme from "../../components/Global/Theme";
+import styled from "styled-components";
+import PageWrapper from "../../components/Global/PageWrapper";
+import { v4 as uuidv4 } from "uuid";
+import React from "react";
+
