5f1c30440b6985346c21d0279c093661a0c1d2a9
Erin Flegg <ehflegg@gmail.com>
2022-08-04 14:10:27 -0700

    map over stockists and add map

---
+++ components/StockistSingle.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import theme from "../components/Global/Theme";
+
+export default function StockistSingle({ stockist, numeral }) {
+  console.log("numeral: ", numeral);
+  return (
+    <div className="stockist--single">
+      <div className="number-name-wrapper">
+        <div className="circle">{numeral}</div>
+        <p>{stockist.acf.stockist_name}</p>
+      </div>
+      <div className="stockist-info">
+        <p>{stockist.acf.stockist_address}</p>
+        <p>{stockist.acf.stockist_phone_number}</p>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={stockist.acf.stockist_website}
+        >
+          <h5 className="website">{stockist.acf.stockist_website}</h5>
+        </a>
+      </div>
+    </div>
+  );
+}
--- pages/stockists.js
+++ pages/stockists.js
@@ -4,94 +4,106 @@ import { getStockists } from "../utils/wordpress";
 import PageWrapper from "../components/Global/PageWrapper";
 import styled from "styled-components";
 import theme from "../components/Global/Theme";
+import StockistSingle from "../components/StockistSingle";
+import { v4 as uuidv4 } from "uuid";
 
 const StockistBlock = styled.div`
-border: solid pink;
-width: 60%;
-max-width: 1000px;
-padding-top: 20px;
-padding-bottom: 20px;
-margin: 10px auto;
-h5.website {
-  font-size: 1.6rem;
+  border: solid pink;
+  width: 60%;
+  max-width: 1000px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  margin: 10px auto;
+  h5.website {
+    font-size: 1.6rem;
 
-  ${theme.mediaQuery.md`
+    ${theme.mediaQuery.md`
     font-size: 2rem;
 `}
-
-}
-h5.province {
-  color: ${theme.colours.gusGreen};
-}
-.circle {
-  display: flex;
-  align-items: center;
-  border-radius: 50%;
-  width: 28px;
-  height: 28px;
-  padding: 10px;
-  background: ${theme.colours.soil};
-  color: ${theme.colours.darkWheat};
-  text-align: center;
-  font-size: 2rem;
-}
-.number-name-wrapper {
-  border: solid teal;
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-p {
-  margin-left: 10px;
-}
-}
-.province-wrapper {
-  border: solid green;
-  width: 100%;
-  positon: relative;
-  left: -38px;
-}
-.stockist-info {
-  position: relative;
-  left: 40px;
-}
-
+  }
+  h5.province {
+    color: ${theme.colours.gusGreen};
+  }
+  .circle {
+    display: flex;
+    align-items: center;
+    border-radius: 50%;
+    width: 28px;
+    height: 28px;
+    padding: 10px;
+    background: ${theme.colours.soil};
+    color: ${theme.colours.darkWheat};
+    text-align: center;
+    font-size: 2rem;
+  }
+  .number-name-wrapper {
+    border: solid teal;
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    p {
+      margin-left: 10px;
+    }
+  }
+  .province-wrapper {
+    border: solid green;
+    width: 100%;
+    positon: relative;
+    left: -38px;
+  }
+  .stockist-info {
+    position: relative;
+    left: 40px;
+  }
+  iframe {
+    border: 0px;
+    margin: 45px 0;
+  }
 `;
 
+const provinces = ["British Columbia", "Alberta", "Saskatchewan"];
 
 export default function Stockists({ stockists }) {
   console.log("stockists: ", stockists);
   return (
     <PageWrapper className="">
       <h1 className="text-center">Stockists</h1>
-      <hr/>
-      <p className="text-center">Pick up the latest issue of Asparagus Magazine at any one of these fine establishments!</p>
+      <hr />
+      <p className="text-center">
+        Pick up the latest issue of Asparagus Magazine at any one of these fine
+        establishments!
+      </p>
       <StockistBlock>
-      <ul>
-        {stockists.map((stockist, index) => {
+        <div className="stockist-map">
+          <iframe
+            src="https://www.google.com/maps/d/u/1/embed?mid=1blqMUcfw2nIdUyHBsWc9diqt3sNqbVKI&ehbc=2E312F"
+            width="100%"
+            height="480"
+          ></iframe>
+        </div>
+        {provinces.map((province, index) => {
           return (
-            <div key={index} className="stockist--single">
+            <React.Fragment key={uuidv4()}>
               <div className="province-wrapper">
-                <h5 className="province">{stockist.acf.province}</h5>
+                <h5 className="province">{province}</h5>
               </div>
-              <div className="number-name-wrapper">
-              <div className="circle">1</div>
-              <p>{stockist.acf.stockist_name}</p>
-              </div>
-              <div className="stockist-info">
-              <p>{stockist.acf.stockist_address}</p>
-              <p>{stockist.acf.stockist_phone_number}</p>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={stockist.acf.stockist_website}
-              >
-                <h5 className="website">{stockist.acf.stockist_website}</h5>
-              </a>
-              </div>
-            </div>
+              <ul>
+                {stockists.map((stockist, stockistIndex) => {
+                  return (
+                    <React.Fragment key={uuidv4()}>
+                      {stockist.acf.province == province ? (
+                        <StockistSingle
+                          stockist={stockist}
+                          numeral={index + 1}
+                        />
+                      ) : null}
+                    </React.Fragment>
+                  );
+                })}
+              </ul>
+            </React.Fragment>
           );
         })}
-      </ul>
       </StockistBlock>
     </PageWrapper>
   );
