6da0ae4a56c524d6ec3b6a99fecc0dca5d20f5e1
Elizabeth Flegg <elizabeth.flegg@gmail.com>
2022-09-02 13:17:08 -0400

    footer menu

--- components/Global/footer.js
+++ components/Global/footer.js
@@ -12,65 +12,92 @@ const FooterContainer = styled.footer`
   max-width: 100%;
   background: ${theme.colours.gusYellow};
   overflow: hidden;
-  padding: 50px 0 50px 50px; 
+  // padding: 50px 0 50px 50px; 
   display: flex; 
-  flex-direction: column; 
-  border: solid black;
+  flex-direction: row; 
+  // border: solid black;
+  .footer-inner {
+    display: flex;
+    flex-direction: column;
+    // border: solid 2px white;
+    width: 100%;
+    ${theme.mediaQuery.md`
+    flex-direction: row;
+    `}
+  }
 `;
 const FooterMenu = styled.div`
   display: flex;
   justify-content: space-between; 
-  border: solid hotpink;
+  // border: solid hotpink;
+  padding: 20px 12px;
+  ${theme.mediaQuery.md`
+  padding: 40px 20px 40px 40px;
+  width: 60%;
+  `}
+  flex-direction: column;
+  .footer-menu--inner {
+    // border: 3px solid yellow;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
   li {
-    padding: 5px;
+    padding: 0 0 5px 0;
     list-style-type: none; 
-    ${theme.mediaQuery.xs`
-    font-size: calc(16px + 2 * ((100vw - 320px) / 680));
-   `}
-    ${theme.mediaQuery.md`
-    @include font-size(1.8);
-   `}
   }
   .nav-link {
     color: ${theme.colours.gusGreen}; 
     font-family: ${theme.type.header};
-    font-size: 2.8rem; 
+    font-size: 1.6rem;
+    ${theme.mediaQuery.md`
+    font-size: 2.8rem;
+    `}
+     
     font-weight: bold; 
   }
   .subnav {
+    // border: solid blue;
     padding-left: 0;
     a {
       font-family: ${theme.type.accent};
-      font-size: 1.8rem; 
+      font-size: 1.3rem; 
+      color: black;
+      ${theme.mediaQuery.md`
+      font-size: 1.8rem;
+      `}
     }
   }
 `;
 
 const SocialMediaLogos = styled.div`
+// border: solid green;
   display: flex; 
+  justify-content: center;
+  img {
+    height: 20px;
+  }
+  
 `;
 
 const NewsletterContainer = styled.div`
-border: solid rebeccapurple;
+// border: solid rebeccapurple;
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+// width: 45%;
 `;
 const SignUp = styled.div`
-  // &.text--support {
-    border: 3px solid rebeccapurple;
-  // }
-  /* border: 3px solid rebeccapurple; */
+  // border: 3px solid salmon;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  /* padding: 30px 0px 20px 20px; */
-  /* ${theme.mediaQuery.sm`
-   padding: 30px;
-  `}
+  padding-right: 10px;
   ${theme.mediaQuery.md`
-  position: relative;
-  left: 40px;
-  padding: 40px;
-  `} */
+  margin-top: 40px;
+  `}
+  
   input {
     height: 38px;
     max-width: 280px;
@@ -87,34 +114,36 @@ const SignUp = styled.div`
  }
  .btn--primary {
    z-index: 1; 
-
- }
- .newsletter-header {
-   font-family: ${theme.type.header};
-   font-size: 2.8rem; 
-   color: ${theme.colours.gusGreen}
- }
- .newsletter-subheader {
-   font-family: ${theme.type.accent};
-   font-size: 2rem; 
-   color: ${theme.colours.soil}; 
-   line-height: 2.4rem; 
+   position: absolute;
+   font-size: 1.4rem;
+   left: 165px;
+   ${theme.mediaQuery.md`
+   left: 990px;
+   font-size: 1.6rem;
+   `}
  }
 `;
 
 const Logo = styled.div`
-  height: 100%;
-  min-width: 40%;
+// border: solid teal;
+width: 100%;
+min-width: 150px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    flex-grow: 1;
+  }
   ${theme.mediaQuery.sm`
   min-width: 30%;
   `}
   ${theme.mediaQuery.md`
-  position: relative;
-  left: -40px;
+  // position: relative;
+  // // left: -40px;
   min-width: 30%;
   `}
 
-  flex-direction: row;
+  // flex-direction: row;
 `;
 
 export default function Footer() {
@@ -139,7 +168,9 @@ export default function Footer() {
 
   return (
     <FooterContainer>
+      <div className="footer-inner">
       <FooterMenu className="footer--container">
+        <div className="footer-menu--inner">
         {footerLinks?.items?.map((link, index) => {
           return (
             <li className="nav-link" key={uuidv4()}>
@@ -184,27 +215,30 @@ export default function Footer() {
             </li>
           );
         })}
+      </div>
+      <SocialMediaLogos>
+        <img src="/twitter_soil.svg" />
+        <img src="/instagram_soil.svg" />
+        <img src="/facebook_soil.svg" />
+      </SocialMediaLogos>
+        </FooterMenu>
         <NewsletterContainer>
           <div className="signupWrapper">
             <SignUp>
-              <p className="newsletter-header">Sign up for News from the Asparagus Patch</p>
-              <p className="newsletter-subheader">Pleasantly infrequent updates from Asparagus Magazine</p>
+              <p className="newsletter-header--footer">Sign up for News from the Asparagus Patch</p>
+              <p className="newsletter-subheader--footer">Pleasantly infrequent updates from Asparagus Magazine</p>
               <div className="input-container">
                 <input type="test" />
                 <button className="btn--primary"> Sign Up </button>
               </div>
             </SignUp>
           </div>
-        </NewsletterContainer>
-        <Logo>
+          <Logo>
           <img src="asparagus_tip_logo.svg" alt="asparagus logo" />
         </Logo>
-      </FooterMenu>
-      <SocialMediaLogos>
-        <img src="/twitter_soil.svg" />
-        <img src="/instagram_soil.svg" />
-        <img src="/facebook_soil.svg" />
-      </SocialMediaLogos>
+        </NewsletterContainer>
+        </div>
+      
     </FooterContainer>
   );
 }
