01a24f980b5da221c91f24b069530daf5d346e3f
Erin Flegg <ehflegg@gmail.com>
2022-10-10 12:19:16 -0700

    add missing apu url to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const Config = {
   // apiUrl: "http://asparagus.local",
-  apiUrl: "https://stage.asparagusmagazine.com",
+  // apiUrl: "https://stage.asparagusmagazine.com",
+  apiUrl: `${process.env.API_URL}`,
 };
