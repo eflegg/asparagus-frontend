27f352a57d6e0fd65838427043e4e1c229b30e6c
Erin Flegg <ehflegg@gmail.com>
2022-10-20 16:36:57 -0700

    add revalidate code

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,5 +1,20 @@
-// import { NextApiRequest, NextApiResponse } from "next";
+export default async function handler(req, res) {
+  const { path, token } = req.query;
 
-// export default async function handler(req, res): Promise {
-//   // our code will go here
-// }
+  if (token !== process.env.REVALIDATION_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  } else if (path.length === 0) {
+    return res.status(401).json({ message: "Path is required" });
+  }
+
+  try {
+    await res.unstable_revalidate(path);
+  } catch (err) {
+    return res.status(500).send("Error revalidating page");
+  }
+
+  return res.status(200).json({
+    revalidated: true,
+    message: `Path ${path} revalidated successfully`,
+  });
+}
