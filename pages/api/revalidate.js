b300b19aa1cfc0e60842fd0be2f24e8bb1946447
Erin Flegg <ehflegg@gmail.com>
2022-10-21 12:55:58 -0700

    add new revalidate snippet

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,20 +1,55 @@
-export default async function handler(req, res) {
-  const { path, token } = req.query;
+// export default async function handler(req, res) {
+//   const { path, token } = req.query;
 
-  if (token !== process.env.REVALIDATION_TOKEN) {
+//   if (token !== process.env.REVALIDATION_TOKEN) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   } else if (path.length === 0) {
+//     return res.status(401).json({ message: "Path is required" });
+//   }
+
+//   try {
+//     await res.unstable_revalidate(path);
+//   } catch (err) {
+//     return res.status(500).send("Error revalidating page");
+//   }
+
+//   return res.status(200).json({
+//     revalidated: true,
+//     message: `Path ${path} revalidated successfully`,
+//   });
+// }
+
+export default async function handler(req, res) {
+  const {
+    body: { paths },
+    method,
+  } = req;
+
+  if (
+    req.headers.authorization !== `Bearer ${process.env.REVALIDATE_SECRET_KEY}`
+  ) {
     return res.status(401).json({ message: "Invalid token" });
-  } else if (path.length === 0) {
-    return res.status(401).json({ message: "Path is required" });
+  }
+
+  if (method !== "PUT") {
+    return res.status(405).json({ message: `Method ${method} Not Allowed` });
+  }
+
+  if (!paths) {
+    return res.status(412).json({ message: "No paths" });
   }
 
   try {
-    await res.unstable_revalidate(path);
-  } catch (err) {
-    return res.status(500).send("Error revalidating page");
-  }
+    const revalidatePaths = paths
+      .filter((path) => path.startsWith("/"))
+      .map((path) =>
+        res.unstable_revalidate(path, { unstable_onlyGenerated: false })
+      );
 
-  return res.status(200).json({
-    revalidated: true,
-    message: `Path ${path} revalidated successfully`,
-  });
+    await Promise.all(revalidatePaths);
+
+    return res.json({ revalidated: true, message: "Paths revalidated" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 }
