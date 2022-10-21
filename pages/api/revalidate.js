2f0b047351fd83c85c5dc101c43e69fd3b2a5fb9
Erin Flegg <ehflegg@gmail.com>
2022-10-21 15:00:54 -0700

    update token

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,3 +1,18 @@
+export default async function handler(req, res) {
+  if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    await res.revalidate(req.query.path);
+    return res.json({
+      revalidated: true,
+    });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
+
 // export default async function handler(req, res) {
 //   const { path, token } = req.query;
 
@@ -19,37 +34,37 @@
 //   });
 // }
 
-export default async function handler(req, res) {
-  const {
-    body: { paths },
-    method,
-  } = req;
+// export default async function handler(req, res) {
+//   const {
+//     body: { paths },
+//     method,
+//   } = req;
 
-  if (
-    req.headers.authorization !== `Bearer ${process.env.REVALIDATE_SECRET_KEY}`
-  ) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+//   if (
+//     req.headers.authorization !== `Bearer ${process.env.REVALIDATE_SECRET_KEY}`
+//   ) {
+//     return res.status(401).json({ message: "Invalid token" });
+//   }
 
-  if (method !== "PUT") {
-    return res.status(405).json({ message: `Method ${method} Not Allowed` });
-  }
+//   if (method !== "PUT") {
+//     return res.status(405).json({ message: `Method ${method} Not Allowed` });
+//   }
 
-  if (!paths) {
-    return res.status(412).json({ message: "No paths" });
-  }
+//   if (!paths) {
+//     return res.status(412).json({ message: "No paths" });
+//   }
 
-  try {
-    const revalidatePaths = paths
-      .filter((path) => path.startsWith("/"))
-      .map((path) =>
-        res.unstable_revalidate(path, { unstable_onlyGenerated: false })
-      );
+//   try {
+//     const revalidatePaths = paths
+//       .filter((path) => path.startsWith("/"))
+//       .map((path) =>
+//         res.unstable_revalidate(path, { unstable_onlyGenerated: false })
+//       );
 
-    await Promise.all(revalidatePaths);
+//     await Promise.all(revalidatePaths);
 
-    return res.json({ revalidated: true, message: "Paths revalidated" });
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-}
+//     return res.json({ revalidated: true, message: "Paths revalidated" });
+//   } catch (err) {
+//     return res.status(500).json({ message: err.message });
+//   }
+// }
