62634297e20240886093d688fbc140d61e1c8cf8
Erin Flegg <ehflegg@gmail.com>
2023-04-13 11:47:04 -0700

    updated wp domain url so that hopefully preview doesn't override staging-new, removed commented out code

--- utils/wordpress.js
+++ utils/wordpress.js
@@ -1,21 +1,6 @@
 // const BASE_URL = "https://stage.asparagusmagazine.com/wp-json/wp/v2";
 const BASE_URL = `${process.env.BASE_URL}`;
 
-/// Posts ///
-
-// export async function getPosts() {
-//   const postsRes = await fetch(BASE_URL + "/posts?_embed");
-//   const posts = await postsRes.json();
-//   return posts;
-// }
-
-// export async function getPost(slug) {
-//   const posts = await getPosts();
-//   const postArray = posts.filter((post) => post.slug == slug);
-//   const post = postArray.length > 0 ? postArray[0] : null;
-//   return post;
-// }
-
 /// Events ///
 
 export async function getEvents() {
